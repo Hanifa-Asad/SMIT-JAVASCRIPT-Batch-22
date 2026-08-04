085d5b9d0dd742ac58b43f8137ea9c491ea11862
Tuba282 <tubajan282@gmail.com>
2026-08-04 10:55:53 +0500

    Chap 6 half

---
+++ Class1/app.js
@@ -0,0 +1,80 @@
+// alert("Hello World");
+// // window.alert("Hello World window");
+// // alert("Welcome To our Website");
+// // prompt("What is your Name?");
+// var name = "Hanifa";
+// console.log(name);
+// console.log();
+// var country = prompt("what is your country name");
+// console.log(country);
+
+// var age;
+// age = 12
+// console.log(age);
+// var country;
+// country = prompt("what is your country name")
+// console.log(country);
+// country = "india"
+// console.log(country);
+// var thanks = "thank u for visit"
+// alert(thanks)
+var num = 10;
+console.log(num);
+
+num = num + 25
+console.log(num);
+
+num += 25
+console.log(num);
+
+var num1 = 20
+var num2 = 30
+var total = num1 + num2;
+console.log(total);
+var number = 20
+var number2 = "20"
+var total = number + number2;
+console.log(total);
+var number = 20
+var number2 = "20"
+var total = number * number2;
+console.log(total);
+var number = 20
+var number2 = "20"
+var total = number / number2;
+console.log(total);
+var number = 20
+var number2 = "20"
+var total = number - number2;
+console.log(total);
+
+var caseQty = 144;
+ alert(caseQty);
+var number = 20
+console.log(number + 20);
+ var camelCaseTotal = 20
+ var snake_case_total = 20;
+ var PascalCaseTotal = 20
+var firstName = "Hanifa"
+var LastName = "Asad"
+var full_name = "HanifaAsad"
+var whatsLeftOver = 10 % 3;
+console.log(whatsLeftOver);
+
+var num = 1;
+var newNum = num++ //2
+ 
+console.log(newNum);//1
+ newNum = num++ ;//3
+
+console.log(newNum);//2
+ newNum = num++ ;//4
+
+console.log(newNum);//3
+ newNum = num++ ;//5
+
+
+console.log(newNum);//4
+
+
+
