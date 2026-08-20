70df6df1bccf655ff1703a675d4f1a68e0d96539
Hanifa-Asad <hanifaasasd23@gmail.com>
2026-08-20 10:56:52 +0500

    chap 19 half and array additional methods

---
+++ Class6/app.js
@@ -0,0 +1,78 @@
+// let students = ["Ali","Sara","Hina"]
+// let newStudents = [
+//     ["Ali",20], // [2][1]
+//     ["Sara", 21],
+//     ["Hina", 18]
+// ]
+
+// console.log(newStudents);
+// let data =[
+//     [
+//         [10,20,30],//0
+//         [40,50,60]//1
+//     ], //0
+//     [
+//         [70,80,90],//0
+//         [100,110,120]//1
+//     ]//1
+// ]
+// console.log(data);
+// console.log(data[1][1][2]);
+//  data[0][0][1] = 85
+// console.log(data);
+// data[0][1][2] = 95
+// console.log(data);
+// data[1][0][2] = 75
+// console.log(data);
+
+
+
+// for(initialization, condition , update){
+//     block of code to repeat
+// }
+for(let i =0; i<=4 ; i++){
+    console.log("hello world");
+    
+}
+// i=0
+// 0<5? true
+// log=0
+// i++ = 1
+
+// i=1
+// 1<5? true
+// log= 1
+// i++ = 2
+//  i=2
+//  2<5 true
+//  log=2
+//  i++ 3
+//  i=3
+//  3<5 true
+//  log 3
+//  i++ =4
+//  i=4
+//  4<5 true
+//  log 4
+//  i++ =4
+//  i=4
+//  i=5
+//  5<5 false
+//  stop
+// for(let i =10; i>=1; i--){
+//     console.log(i);
+    
+// }
+
+for(let i=1; i<=10; i++){
+    if(i%2 ===0){
+        console.log(i);
+        
+    }
+
+}
+let fruits = ["Apple", "Banana", "Mango","Cherry", "Orange"]
+for(let i =0 ; i<fruits.length; i++){
+    console.log(fruits[i]);
+    
+}
