31a8d6938c3e87711fd7a05714d330e5755ff200
Hanifa-Asad <hanifaasasd23@gmail.com>
2026-09-10 11:00:09 +0500

    chap 39 complete

---
+++ Class12/app.js
@@ -0,0 +1,118 @@
+// console.log("Hello Hanifa");
+// console.log("Hello Hanifa");
+// console.log("Hello Hanifa");
+
+
+
+// function sayHello() {
+//     console.log("Hello hANIFA");
+    
+// } // FUNCTION DEFINITION
+
+// sayHello();
+// sayHello() // FUNCTION CALL/INVOKE
+
+// function greet(name) {
+//     console.log("Hello", name);
+    
+// }
+// greet("hanifa");
+// greet("Huma");
+
+// function add(a,b){
+//     console.log(a+b);
+    
+// }
+// add(50,25);
+// add(30,20);
+
+// function multiply(a,b){
+  
+//     return a*b
+//       console.log(a*b);
+// }
+// multiply(10,20);
+
+// let result = multiply(10,30)
+// console.log(result);
+
+
+// function checkAge(age){
+//     if(age>= 18){
+//         return "Adult";
+
+//     }else{
+//         return "minor"
+//     }
+// }
+// console.log(checkAge(25));
+
+
+
+// function showStudents(students){
+//     for(let i=0; i<students.length; i++){
+//           console.log(students[i]);
+//     }
+
+
+        
+// }
+// let names = ["Ali", "Sara", "Sana","Dua"];
+// showStudents(names);
+
+
+
+// let name = "Hanifa";
+// function greet(){
+//     console.log(name);
+    
+// }
+// greet();
+// console.log(name);
+
+// function greet(){
+//     let name = "hanifa";
+//     console.log(name);
+    
+// }
+// greet();
+// console.log(name);
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let day = 1
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("tuesday");
+        break;
+        
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Invalid Day");
+            
+            
+}
+
+let num1 = 20;
+let num2 = 5;
+let operator = "+";
+
+switch (operator){
+    case"+"
+    console.log(num1+num2);
+    
+}
